e29243ea79d218b4df2ab8ecbb70bf2ce55c64f7
Ali Kozan <ankozan@mail.fhsu.edu>
2023-04-22 09:52:21 -0700

    get routes check

--- server.js
+++ server.js
@@ -20,6 +20,60 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+const stateAbbreviations = {
+    "AL": "ALABAMA",
+    "AK": "ALASKA",
+    "AZ": "ARIZONA",
+    "AR": "ARKANSAS",
+    "CA": "CALIFORNIA",
+    "CO": "COLORADO",
+    "CT": "CONNECTICUT",
+    "DE": "DELAWARE",
+    "FL": "FLORIDA",
+    "GA": "GEORGIA",
+    "HI": "HAWAII",
+    "ID": "IDAHO",
+    "IL": "ILLINOIS",
+    "IN": "INDIANA",
+    "IA": "IOWA",
+    "KANSAS": "KS",
+    "KY": "KENTUCKY",
+    "LA": "LOUISIANA",
+    "ME": "MAINE",
+    "MD": "MARYLAND",
+    "MA": "MASSACHUSETTS",
+    "MI": "MICHIGAN",
+    "MN": "MINNESOTA",
+    "MS": "MISSISSIPPI",
+    "MO": "MISSOURI",
+    "MT": "MONTANA",
+    "NE": "NEBRASKA",
+    "NV": "NEVADA",
+    "NH": "NEW HAMPSHIRE",
+    "NJ": "NEW JERSEY",
+    "NM": "NEW MEXICO",
+    "NY": "NEW YORK",
+    "NC": "NORTH CAROLINA",
+    "ND": "NORTH DAKOTA",
+    "OH": "OHIO",
+    "OK": "OKLAHOMA",
+    "OR": "OREGON",
+    "PA": "PENNSYLVANIA",
+    "RI": "RHODE ISLAND",
+    "SC": "SOUTH CAROLINA",
+    "SD": "SOUTH DAKOTA",
+    "TN": "TENNESSEE",
+    "TX": "TEXAS",
+    "UT": "UTAH",
+    "VT": "VERMONT",
+    "VA": "VIRGINIA",
+    "WA": "WASHINGTON",
+    "WV": "WEST VIRGINIA",
+    "WI": "WISCONSIN",
+    "WY": "WYOMING"
+};
+
+
 let statesWithFunFacts;
 let states;
 async function getStatesWithFunFacts() {
@@ -42,6 +96,8 @@ async function run() {
     await getStatesWithFunFacts();
 }
 run().catch(console.dir);
+
+
 
 app.get('/states', async (req, res) => {
     try {
@@ -74,18 +130,23 @@ app.get('/states', async (req, res) => {
 
 app.get('/states/:state', async (req, res) => {
     await getStatesWithFunFacts();
-
     let filteredArray
     const stateCode = req.params.state.toUpperCase();
-    console.log(stateCode);
-    filteredArray = statesWithFunFacts.find(state => state.code === stateCode);
+    if (!(stateCode in stateAbbreviations)) {
 
-    if (filteredArray) {
-        const formattedData = JSON.stringify(filteredArray, null, 2);
-        res.set('Content-Type', 'application/json');
-        res.send(formattedData);
+        console.log(stateCode)
+        filteredArray = statesWithFunFacts.find(state => state.code === stateCode);
+
+        if (filteredArray) {
+            const formattedData = JSON.stringify(filteredArray, null, 2);
+            res.set('Content-Type', 'application/json');
+            res.send(formattedData);
+        } else {
+            res.status(404).send('State not found');
+        }
     } else {
-        res.status(404).send('State not found');
+        res.status(404).json({ "message": "Invalid state abbreviation parameter" });
+
     }
 });
 
@@ -93,14 +154,21 @@ app.get('/states/:state', async (req, res) => {
 
 app.get('/states/:state/funfact', async (req, res) => {
     connectToDB();
+    await getStatesWithFunFacts();
 
     const stateCode = req.params.state.toUpperCase();
-    const stateData = await States.findOne({ stateCode: stateCode });
-
+    const stateData = statesWithFunFacts.find(state => state.code === stateCode);
     if (stateData) {
-        const formattedData = JSON.stringify(stateData.funfacts[Math.floor(Math.random() * stateData.funfacts.length)], null, 2);
-        res.set('Content-Type', 'application/json');
-        res.send(formattedData);
+        if (stateData.funfacts.length > 0) {
+            const formattedData = JSON.stringify(stateData.funfacts[Math.floor(Math.random() * stateData.funfacts.length)], null, 2);
+            res.set('Content-Type', 'application/json');
+            res.send(formattedData);
+        }
+        else {
+            res.status(404).send('No Fun Facts found for ' + stateData.state);
+
+        }
+
     } else {
         res.status(404).send('State not found');
     }
@@ -188,23 +256,9 @@ app.get('/states/:state/admission', async (req, res) => {
     }
 });
 
-app.get('/', async (req, res) => {
-    try {
-        const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        const myDatabase = client.db('backendfinal');
-        const myCollection = myDatabase.collection('states');
-        // Find all documents in the collection
-        const documents = await myCollection.find({ stateCode: 'KS' }).toArray();
-        res.send(documents);
-    }
-    catch (err) {
-        console.error(err);
-        res.status(500).send('Server Error');
-    } finally {
-        await mongoose.connection.close();
-    }
-
-})
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404<h1>');
+});
 
 
 
